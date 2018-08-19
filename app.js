46b731f140fabaafc3b67d4378c3f835c9f4342e
djamesp1 <24489431+djamesp1@users.noreply.github.com>
2018-08-19 08:53:50 -0700

    Encoding portion of HTTP req

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+     a: {
+       demand: true,
+       alias: 'address',
+       describe: 'Address to fetch weather for',
+       string: true
+     }
+   })
+   .help()
+   .alias('help', 'h')
+   .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+console.log(encodedAddress);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyBGpQL47MjLDAWN-s4tRPryJfqSChkAfcc',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(response, undefined, 2));
